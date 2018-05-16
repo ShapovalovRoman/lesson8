5d7859eb32cef236fbc1548d5ed0bfc40ade2ec6
ShapovalovRoman <r.f.shapovalov@gmail.com>
2018-05-16 13:31:24 +0300

    add

---
+++ js/lesson4.js
@@ -0,0 +1,39 @@
+// var User = function(name, username){
+//     this.name = name;
+//     this.surname = surname;
+//     this.greet = function(){
+
+//     }
+// }
+
+// var user1 = new User('bob','brown');
+
+// var human = {
+//     breath: true
+// }
+
+// var man = {
+//     eat = true
+// }
+
+// var woman = {
+//     eat = false
+// }
+
+
+// man._proto_ = human;
+// woman._proto_ = human;
+//console.log('hello');
+
+// let 
+// const 
+
+// 12 задание
+
+//let a = document.getElementsByTagName('div');
+
+//console.log(a);
+
+var a = document.getElementsByName('div');
+
+console.log(a);
